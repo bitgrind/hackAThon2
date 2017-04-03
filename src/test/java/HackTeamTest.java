9d6d2bef71a4a0dfd2a24be03066a6281ce613db
Guest User <Guest@Epicodus-5A.local>
2017-04-03 16:41:04 -0700

    added team getters

--- src/test/java/HackTeamTest.java
+++ src/test/java/HackTeamTest.java
@@ -1,4 +1,5 @@
 import org.junit.*;
+import java.util.List;
 import static org.junit.Assert.*;
 
 public class HackTeamTest {
@@ -16,5 +17,26 @@ public class HackTeamTest {
     newTeam.addMember(newMember);
     assertTrue(newTeam.getMembers().contains(newMember));
   }
-  
+
+  @Test
+  public void HackTeam_Addmember_true() {
+    HackTeam newTeam = new HackTeam("Blossom");
+    HackMember newMember = new HackMember("keith", "java");
+    newTeam.addMember(newMember);
+    assertEquals(true, newTeam.getMembers() instanceof List<?>);
+  }
+
+  @Test
+  public void HackTeam_allteams_true(){
+    HackTeam newTeam = new HackTeam("Blossom");
+    assertTrue(newTeam.allTeams() instanceof List<?>);
+  }
+
+  @Test
+  public void test_getters(){
+    HackTeam newTeam = new HackTeam("Blossom");
+    assertEquals(0, newTeam.getId());
+    assertEquals("Blossom", newTeam.getName());
+  }
+
 }
